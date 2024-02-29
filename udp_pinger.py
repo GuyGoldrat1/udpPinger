b4e2de531ed6fe4b6fb0074ec2444c7dc4a661d3
guy <guygoldrat@gmail.com>
2024-02-29 10:17:07 +0200

    asd

--- udp_pinger.py
+++ udp_pinger.py
@@ -10,19 +10,21 @@ def pinger():
     port = int(sys.argv[4]) if len(sys.argv) > 4 and sys.argv[3] == '-p' else 1337
     size = int(sys.argv[6]) if len(sys.argv) > 6 and sys.argv[5] == '-s' else 100
     count = int(sys.argv[8]) if len(sys.argv) > 8 and sys.argv[7] == '-c' else 10
-    timeout = int(sys.argv[10]) if len(sys.argv) > 10 and sys.argv[9] == '-t' else 1000
+    timeout = int(sys.argv[10]) if len(sys.argv) > 10 and sys.argv[9] == '-t' else 1
 
     # Create a UDP socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.settimeout(timeout)
         sequence_number = 0
+        resived = 0
 
         for i in range(count):
             send_time = time.time()
             message_id = sequence_number
 
             # Create ping request
-            ping_request = create_ping_request(message_id)
+            ping_request = create_ping_request(message_id, size)
+            print(ping_request)
             sock.sendto(ping_request, (ip_address, port))
 
             try:
@@ -32,7 +34,8 @@ def pinger():
 
                 if opcode == 1 and reply_id == message_id:
                     rtt = round((recv_time - send_time) * 1000, 3)
-                    print(f"{len(data)} bytes from {ip_address}: seq={sequence_number} rtt={rtt} ms")
+                    resived+=1
+                    print(f"{sys.getsizeof(data) + sys.getsizeof(address)} bytes from {ip_address}: seq={sequence_number} rtt={rtt} ms")
                 else:
                     print(f"Received invalid reply for sequence {sequence_number}")
 
@@ -41,12 +44,12 @@ def pinger():
 
             sequence_number += 1
 
-        print(f"---{ip_address} statistics--- \n{count} packets transmitted, {sequence_number} packets received, "
-              f"{((sequence_number - count) / sequence_number) * 100:.1f}% packet loss")
+        print(f"---{ip_address} statistics--- \n{sequence_number} packets transmitted, {resived} packets received, "
+              f"{((sequence_number - resived) / sequence_number) * 100:.1f}% packet loss")
 
-def create_ping_request(message_id):
+def create_ping_request(message_id, size):
     opcode = 0
-    payload = "Hello, Pinger!"
+    payload = "A" * size
     ping_request = bytes([opcode]) + message_id.to_bytes(4, byteorder='big') + payload.encode('utf-8')
     return ping_request
 
